4061e6b9dd5ea07f56caf6234584e71c117051ff
Akash Tiwari <akashtiwari48200@gmail.com>
2022-02-14 12:07:49 +0530

    User input

--- train.py
+++ train.py
@@ -5,4 +5,10 @@ Created on Mon Feb 14 11:38:08 2022
 @author: Aakash Tiwari
 """
 
-print("This is a Training file")
+t=input("Do you need Training?")
+if t=="yes":
+    print("Let's start the training")
+elif t=="no":
+    print("Already trained, nice!")
+else:
+    print("Please give a valid response")
