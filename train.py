96647818a1f26987ed8c2d8faccb6a86e9c7c460
Akash Tiwari <akashtiwari48200@gmail.com>
2022-02-15 11:34:54 +0530

    new message added

--- train.py
+++ train.py
@@ -8,7 +8,12 @@ Created on Mon Feb 14 11:38:08 2022
 t=input("Do you need Training?")
 if t=="yes":
     print("Let's start the training")
+    x=int(input("how many months of training will you like to have?"))
+    print("The cost of that would be {}".format(x*1000))
 elif t=="no":
     print("Already trained, nice!")
 else:
     print("Please give a valid response")
+
+
+
